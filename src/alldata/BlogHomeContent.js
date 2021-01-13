91739e1d596d8c2d652cc4314273fe6f04e92b13
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-13 21:27:27 +0900

    Update BlogHomeContent.js

--- src/alldata/BlogHomeContent.js
+++ src/alldata/BlogHomeContent.js
@@ -2,5 +2,5 @@ import bloghima from "../images/bloghima.png";
 
 export const BlogHomeContent = {
     'img': bloghima,
-    'desc':'tes123tes123'
+    'desc':'Great stories that I would like to share with you about my daily life and my journey. Scroll down to take a look. Enjoy!'
 }
