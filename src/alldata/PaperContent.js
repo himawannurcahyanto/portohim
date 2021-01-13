d62ef09ea4d0fde1ecafbc0a49015974864ed044
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-13 23:43:57 +0900

    Update PaperContent.js

--- src/alldata/PaperContent.js
+++ src/alldata/PaperContent.js
@@ -3,22 +3,22 @@ import Paper1 from "../images/Transformer-Isolation-Oil-Testing-Standards.png";
 
 export const PaperData = [
     {
-        title: "Energy Management by Scheduling ESS with Active Demand Response in Low Voltage Grid",
-        tipe: "Conference Paper",
-        text: null,
-        date: "October 2020, 2020 International Conference on Information and Communication Technology Convergence (ICTC)",
-        author: "Md Morshed Alam, Md. Habibur Rahman, Himawan Nurcahyanto, Yeong Min Jang",
-        papimage: null,
-        link: "https://www.researchgate.net/profile/Himawan_Nurcahyanto"
-    },
-    {
-        title: "Smart Factory Based on IoT Platform",
+        title: "Wind Speed Interval Forecasting Under Uncertainty Quantification Pattern Based on Deep Learning Method",
         tipe: "Conference Paper",
         text: "Full-Text Available",
-        date: "September 2020, KICS Summer Conference 2020",
-        author: "Aji Teguh, Himawan Nurcahyanto, Yeong Min",
+        date: "December 2020, Korean Artificial Intelligence Conference 2020",
+        author: "Himawan Nurcahyanto, Aji Teguh Prihatno, Yeong Min Jang",
         papimage: Paper,
-        link: "https://www.researchgate.net/profile/Himawan_Nurcahyanto"
+        link: "http://manuscriptlink-society-file.s3.amazonaws.com/kics/conference/koreaai/presentation/E-2-2.pdf"
+    },
+    {
+        title: "Artificial Intelligence Forecasting for Photovoltaic Performance",
+        tipe: "Conference Paper",
+        text: "Full-Text Available",
+        date: "September 2020, The Korean Institutes of Communications and Information Sciences (KICS)",
+        author: "Himawan Nurcahyanto, Yeong Min Jang",
+        papimage: Paper,
+        link: "https://www.semanticscholar.org/paper/Artificial-Intelligence-Forecasting-for-Performance-Nurcahyanto/6061ad01f758d675bf08293c4245c1e124b4821b"
     },
     {
         title: "Analysis of Power Transformer's Lifetime Using Health Index Transformer Method Based on Artificial Neural Network Modeling",
