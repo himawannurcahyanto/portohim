4d4bb4f20d1a37bf891b38a2915c48a5024948e1
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-13 21:59:19 +0900

    Update ExperienceHomeContent.js

--- src/alldata/ExperienceHomeContent.js
+++ src/alldata/ExperienceHomeContent.js
@@ -2,5 +2,5 @@ import ExpHima from "../images/ExpHima.png";
 
 export const ExpHomeContent = {
     'img': ExpHima,
-    'desc':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris'
+    'desc':'Every experience in my life is designed to teach me something that I need to survive. My experience thought me a lot. I should understand to move forward. I am happy to be a learner. Keep learning and do my best for every process in my whole life.'
 }
