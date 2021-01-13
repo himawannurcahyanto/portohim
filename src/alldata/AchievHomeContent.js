02ceef9cc1277f204ee9005c268ba4a8bab81e0b
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-13 23:16:38 +0900

    Update AchievHomeContent.js

--- src/alldata/AchievHomeContent.js
+++ src/alldata/AchievHomeContent.js
@@ -2,5 +2,5 @@ import achhima from "../images/achhima.png";
 
 export const AchHomeContent = {
     'img':achhima,
-    'desc':'Opportunities will not come to you, You create it! I find that the harder I work, the more luck I seem to have. - Thomas Jefferson'
+    'desc':'Opportunities will not come to you, You create it! I found that the harder I work, the more luck I seem to have. - Thomas Jefferson'
 }
