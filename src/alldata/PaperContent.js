26e57e70d084ca399f906fb95f14482ed7200b87
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-14 02:22:50 +0900

    Update PaperContent.js

--- src/alldata/PaperContent.js
+++ src/alldata/PaperContent.js
@@ -1,14 +1,25 @@
-import Paper from "../images/SmartFactoryBasedonIoTPlatform-pages-1_page-0001 1.png";
-import Paper1 from "../images/Transformer-Isolation-Oil-Testing-Standards.png";
+import Paper1 from "../images/kicswinterhima.png";
+import Paper2 from "../images/kaichima.png";
+import Paper3 from "../images/kicssummerhima.png";
+import Paper4 from "../images/iceeihima.png";
 
 export const PaperData = [
+    {
+        title: "Photovoltaic Power Generation Prediction based on Recurrent Neural Network",
+        tipe: "Conference Paper",
+        text: "Full-Text Available",
+        date: "January 2021, The Korean Institutes of Communications and Information Sciences (KICS)",
+        author: "Himawan Nurcahyanto, Aji Teguh Prihatno, Yeong Min Jang",
+        papimage: Paper1,
+        link: null
+    },
     {
         title: "Wind Speed Interval Forecasting Under Uncertainty Quantification Pattern Based on Deep Learning Method",
         tipe: "Conference Paper",
         text: "Full-Text Available",
         date: "December 2020, Korean Artificial Intelligence Conference 2020",
         author: "Himawan Nurcahyanto, Aji Teguh Prihatno, Yeong Min Jang",
-        papimage: null,
+        papimage: Paper2,
         link: "http://manuscriptlink-society-file.s3.amazonaws.com/kics/conference/koreaai/presentation/E-2-2.pdf"
     },
     {
@@ -17,7 +28,7 @@ export const PaperData = [
         text: "Full-Text Available",
         date: "September 2020, The Korean Institutes of Communications and Information Sciences (KICS)",
         author: "Himawan Nurcahyanto, Yeong Min Jang",
-        papimage: Paper,
+        papimage: Paper3,
         link: "https://www.semanticscholar.org/paper/Artificial-Intelligence-Forecasting-for-Performance-Nurcahyanto/6061ad01f758d675bf08293c4245c1e124b4821b"
     },
     {
@@ -26,7 +37,7 @@ export const PaperData = [
         text: "PDF Available",
         date: "July 2019, International Conference on Electrical Engineering and Informatics (ICEEI)",
         author: "Himawan Nurcahyanto, Jannus Maurits, I Made Ardita, Chairul Hudaya",
-        papimage: Paper1,
+        papimage: Paper4,
         link: "https://www.researchgate.net/publication/339174658_Analysis_of_Power_Transformer's_Lifetime_Using_Health_Index_Transformer_Method_Based_on_Artificial_Neural_Network_Modeling"
     },
 ]
