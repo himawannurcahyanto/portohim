6988434cd3d27931b46c161b1fd5b178b50fe701
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-20 01:24:55 +0900

    Update OrganizationContent.js

--- src/alldata/OrganizationContent.js
+++ src/alldata/OrganizationContent.js
@@ -1,37 +1,60 @@
-import kics from "../images/kics.png";
-import kics1 from "../images/kics1.png";
-import kics2 from "../images/kics2.png";
+import perpika from "../images/logoperpika.png";
+import teknika from "../images/logoteknika.jpg";
+import imeftui from "../images/logoimeftui.png";
+import exercise from "../images/logoexercise.jpg";
+import ieee from "../images/logoieee.png";
+import fkhmei from "../images/logofkhmei.png";
+import trui from "../images/logotrui.jpg";
+import bemftui from "../images/logobemftui.jpg";
 
 export const OrganizationData = [
     {
-        imgMain: kics,
+        imgMain: perpika,
         title: "PERPIKA 2020/2021 (Persatuan Pelajar Indonesia Korea Selatan)",
         position:"External Division",
         date: "September 2020 - September 2021",
         desc:"testestes",
-        img1: kics1,
-        img2: kics2,
+        img1: null,
+        img2: null,
     },
     {
-        imgMain: kics,
+        imgMain: ieee,
+        title: "IEEE Student Branch Universitas Indonesia",
+        position:"Member",
+        date: "January 2018 - December 2018",
+        desc:"testestes",
+        img1: null,
+        img2: null,
+    },
+    {
+        imgMain: exercise,
+        title: "EXERCISE FTUI",
+        position:"Public Relation",
+        date: "January 2018 - December 2018",
+        desc:"testestes",
+        img1: null,
+        img2: null,
+    },
+    {
+        imgMain: teknika,
         title: "TEKNIKA FTUI 2018 (Journalist Organization)",
         position:"Coordinator of Public Relation and Marketing Communication Strategy",
         date: "January 2018 - December 2018",
         desc:"testestes",
-        img1: kics1,
-        img2: kics2,
+        img1: null,
+        img2: null,
     },
     {
-        imgMain: kics,
+        imgMain: imeftui,
         title: "IME FTUI 2017 (Himpunan Mahasiswa Elektro Universitas Indonesia)",
         position:"Ketua Himpunan",
         date: "January 2017 - December 2017",
         desc:"testestes",
-        img1: kics1,
-        img2: kics2,
+        img1: null,
+        img2: null,
     },
     {
-        imgMain: kics,
+        imgMain: fkhmei,
         title: "FKHMEI (Forum Komunikasi Mahasiswa Elektro Indonesia)",
         position:"Delegation of Universitas Indonesia",
         date: "January 2017 - December 2017",
@@ -40,7 +63,7 @@ export const OrganizationData = [
         img2: kics2,
     },
     {
-        imgMain: kics,
+        imgMain: bemftui,
         title: "BEM FTUI 2016 (Badan Eksekutif Mahasiswa FTUI)",
         position:"Pengabdian Masyarakat Division",
         date: "January 2016 - December 2016",
@@ -49,7 +72,7 @@ export const OrganizationData = [
         img2: kics2,
     },
         {
-        imgMain: kics,
+        imgMain: trui,
         title: "TRUI 2016 (Team Robotika Universitasi Indonesia)",
         position:"Legged Robot Division",
         date: "January 2016 - December 2016",
