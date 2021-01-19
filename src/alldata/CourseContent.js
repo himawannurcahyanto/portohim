519a2bb5cbbf55d25941d5d3f128440ca8e403cd
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-20 00:28:26 +0900

    Update CourseContent.js

--- src/alldata/CourseContent.js
+++ src/alldata/CourseContent.js
@@ -1,7 +1,32 @@
 export const CourseData = [
     {
-        course : "Lorem",
-        date : "9990-90-09",
-        credit : "Ipsum",
+        course : "Energy Business Model",
+        date : "September - December",
+        credit : "3",
+    },
+    {
+        course : "ICT (Information and Communications Technology) Convergence",
+        date : "September - December",
+        credit : "3",
+    },
+    {
+        course : "Machine Learning and Deep Learning",
+        date : "September - December",
+        credit : "3",
+    },
+    {
+        course : "Power System Conversion for PV Systems",
+        date : "March - July",
+        credit : "3",
+    },
+    {
+        course : "Renewable Energy",
+        date : "March - July",
+        credit : "3",
+    },
+    {
+        course : "ICT (Information and Communications Technology) Convergence",
+        date : "March - July",
+        credit : "3",
     },
 ]
