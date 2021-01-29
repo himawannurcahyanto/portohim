6be7c86922a5d492dfdc79f65c580ab9db30007b
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-01-30 01:39:29 +0900

    Update BannerSliderContent.js

--- src/alldata/BannerSliderContent.js
+++ src/alldata/BannerSliderContent.js
@@ -1,10 +1,11 @@
 
 export const BannerSliderContent = [
-    {source: "https://drive.google.com/uc?id=1ZCKbArzOMOSQaqRhF_P439VRJ_8S2czB"},
-    {source: "https://drive.google.com/uc?id=1qRxf5T5atXfXe6g1S-DglHLl_7GMdJlp"},
-    {source: "https://drive.google.com/uc?id=1CuxCjhVDU81EDyBpBygenvjJq85yeHAx"},
-    {source: "https://drive.google.com/uc?id=1M4I0uw1nltgHKAjuRkNrRELtSX5WW9R-"},
-    {source: "https://drive.google.com/uc?id=10A9s7wqSPKkWrvNIF8-Xvv8V5-iHt99J"},
-    {source: "https://drive.google.com/uc?id=1hZt9wmDYrtjwBUBiNYyj4M2otc5AyoVx"},
+    {source: "https://drive.google.com/uc?id=1adSHfFevuDrWzu5IowWeIUpdDrhAk7vQ"},
+    {source: "https://drive.google.com/uc?id=1VjA6QV-YpPkFxrUWafgVuqxBBqnml6ed"},
+    {source: "https://drive.google.com/uc?id=1dmPe6vJkFmqERPqT4BCWOGmV5SgPSDtU"},
+    {source: "https://drive.google.com/uc?id=1Yrcwj65IMhAT23FUfBi-GpU7YuGWJarm"},
+    {source: "https://drive.google.com/uc?id=1XYUcSDKuX-C3Hvp1ebTBIj8SK7bBDovH"},
+    {source: "https://drive.google.com/uc?id=1s9d6uCuxkgl01SK6tKYLaqI0HFIsGyqC"},
+    {source: "https://drive.google.com/uc?id=1MuheiUi9EasKwZ2H1aDXVukb447JkORf"},
 
 ]
