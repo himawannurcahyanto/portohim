70099e1a684d908fcdb8ebfa445ded9ad002fe44
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-02-09 03:32:52 +0900

    Update BannerSliderContent.js

--- src/alldata/BannerSliderContent.js
+++ src/alldata/BannerSliderContent.js
@@ -2,31 +2,31 @@
 export const BannerSliderContent = [
     {
         "source": "https://drive.google.com/uc?id=1adSHfFevuDrWzu5IowWeIUpdDrhAk7vQ",
-        "small":"https://www.cruisetradenews.com/wp-content/uploads/2019/11/CMV-PacificAria-TransOcean-Livery-RGB1.jpg",
+        "small":"https://drive.google.com/uc?id=1CmfmBcRf0BvpQMDRkPL6gf38tVEkOJ-k",
     },
     {
         "source": "https://drive.google.com/uc?id=1VjA6QV-YpPkFxrUWafgVuqxBBqnml6ed",
-        "small":"https://www.cruisetradenews.com/wp-content/uploads/2019/11/CMV-PacificAria-TransOcean-Livery-RGB1.jpg",
+        "small":"https://drive.google.com/uc?id=1Lk261gALDj_t4cQ9RheD_YksUgs7l8-K",
     },
     {
         "source": "https://drive.google.com/uc?id=1dmPe6vJkFmqERPqT4BCWOGmV5SgPSDtU",
-        "small":"https://www.cruisetradenews.com/wp-content/uploads/2019/11/CMV-PacificAria-TransOcean-Livery-RGB1.jpg",
+        "small":"https://drive.google.com/uc?id=1Gk5ZtwDooTnJVfQj-IUx0Tm_Zb4J0BUK",
     },
     {
         "source": "https://drive.google.com/uc?id=1Yrcwj65IMhAT23FUfBi-GpU7YuGWJarm",
-        "small":"https://drive.google.com/uc?id=1Yrcwj65IMhAT23FUfBi-GpU7YuGWJarm",
+        "small":"https://drive.google.com/uc?id=1XcWNk6-vdMUzfnoS7p2k1uz8qli3avXo",
     },
     {
         "source": "https://drive.google.com/uc?id=1XYUcSDKuX-C3Hvp1ebTBIj8SK7bBDovH",
-        "small":null,
+        "small":"https://drive.google.com/uc?id=1-itaDoj49W9pAzcMfVxnIklpFq4_Kgps",
     },
     {
         "source": "https://drive.google.com/uc?id=1s9d6uCuxkgl01SK6tKYLaqI0HFIsGyqC",
-        "small":null,
+        "small":"https://drive.google.com/uc?id=1xORK7o5XRpOEwBlKCquPWGV9upco4DoY",
     },
     {
         "source": "https://drive.google.com/uc?id=1MuheiUi9EasKwZ2H1aDXVukb447JkORf",
-        "small":null,
+        "small":"https://drive.google.com/uc?id=1eRRrppDWBI_espHMAphXQIQg-Smzr47o",
     },
 
 ]
