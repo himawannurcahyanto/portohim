16b4febbfcfdefaf5a43c463424765a2b4951b0b
himawannurcahyanto <himawanurcahyanto@gmail.com>
2021-02-09 04:16:36 +0900

    Update BannerSliderContent.js

--- src/alldata/BannerSliderContent.js
+++ src/alldata/BannerSliderContent.js
@@ -1,32 +1,40 @@
+import banner1 from "../images/KICSSUMMERHP.png";
+import banner2 from "../images/COMPFESTHP.png";
+import banner3 from "../images/ICEEIHP.png";
+import banner4 from "../images/KULIAHGOHP.png";
+import banner5 from "../images/BOGORSOCIALACTIONHP.png";
+import banner6 from "../images/SOLARHP.png";
+import banner7 from "../images/JASARAHARJAHP.png";
+
 
 export const BannerSliderContent = [
     {
         "source": "https://drive.google.com/uc?id=1adSHfFevuDrWzu5IowWeIUpdDrhAk7vQ",
-        "small":"https://drive.google.com/uc?id=1CmfmBcRf0BvpQMDRkPL6gf38tVEkOJ-k",
+        "small":banner1,
     },
     {
         "source": "https://drive.google.com/uc?id=1VjA6QV-YpPkFxrUWafgVuqxBBqnml6ed",
-        "small":"https://drive.google.com/uc?id=1Lk261gALDj_t4cQ9RheD_YksUgs7l8-K",
+        "small":banner2,
     },
     {
         "source": "https://drive.google.com/uc?id=1dmPe6vJkFmqERPqT4BCWOGmV5SgPSDtU",
-        "small":"https://drive.google.com/uc?id=1Gk5ZtwDooTnJVfQj-IUx0Tm_Zb4J0BUK",
+        "small":banner3",
     },
     {
         "source": "https://drive.google.com/uc?id=1Yrcwj65IMhAT23FUfBi-GpU7YuGWJarm",
-        "small":"https://drive.google.com/uc?id=1XcWNk6-vdMUzfnoS7p2k1uz8qli3avXo",
+        "small":banner4,
     },
     {
         "source": "https://drive.google.com/uc?id=1XYUcSDKuX-C3Hvp1ebTBIj8SK7bBDovH",
-        "small":"https://drive.google.com/uc?id=1-itaDoj49W9pAzcMfVxnIklpFq4_Kgps",
+        "small":banner5,
     },
     {
         "source": "https://drive.google.com/uc?id=1s9d6uCuxkgl01SK6tKYLaqI0HFIsGyqC",
-        "small":"https://drive.google.com/uc?id=1xORK7o5XRpOEwBlKCquPWGV9upco4DoY",
+        "small":banner6,
     },
     {
         "source": "https://drive.google.com/uc?id=1MuheiUi9EasKwZ2H1aDXVukb447JkORf",
-        "small":"https://drive.google.com/uc?id=1eRRrppDWBI_espHMAphXQIQg-Smzr47o",
+        "small":banner7,
     },
 
 ]
