f940d259fad9c03936b8188548f5194018bc44d6
himawannurcahyanto <himawanurcahyanto@gmail.com>
2022-01-23 17:29:03 +0700

    Update BlogContent.js

--- src/alldata/BlogContent.js
+++ src/alldata/BlogContent.js
@@ -20,6 +20,24 @@ import podcastprofbud1 from "../images/podcastprofbud1.png";
 export const BlogData = [
     {
         imagee: podcastprofbud1,
+        title: "PODCAST LUMAD EPS 1",
+        description:"Keluhan Muda Mudi or abbreviated as LUMAD is an inspirational podcast channel to open the minds of the younger generation. In this podcast, LUMAD invites one of the speakers, Himawan Nurcahyanto. He has completed his master's degree in South Korea and currently working as a management trainee at a state-owned enterprise (BUMN) in Indonesia. The purpose of this podcast is to provide colleagues with an insight about Master's Degree in Korea and also working in conditions of ordinary people that it is still possible for you to achieve. Hope it is useful! Enjoy!",
+        content:{
+            par1 : "Keluhan Muda Mudi or abbreviated as LUMAD is an inspirational podcast channel to open the minds of the younger generation. In this podcast, LUMAD invites one of the speakers, Himawan Nurcahyanto. He has completed his master's degree in South Korea and currently working as a management trainee at a state-owned enterprise (BUMN) in Indonesia. The purpose of this podcast is to provide colleagues with an insight about Master's Degree in Korea and also working in conditions of ordinary people that it is still possible for you to achieve. Hope it is useful for you guys!",
+            par2 : "Enjoy the video!"
+        },
+        image:{
+            par1: null,
+        },
+        videos: {
+            par2: true,
+        },
+        video:{
+            par2: "https://www.youtube.com/embed/yQYaGWjzOcQ",
+        },
+    },
+    {
+        imagee: podcastprofbud1,
         title: "Budimansyah Podcast",
         description:"Hello everyone, on this occasion I was invited by Professor Budimansyah from the Indonesian University of Education, to share my experiences and talk casually about Artificial Intelligence, Internet of Energy, and also Renewable Energy in South Korea. Not only explaining those topics but also I was invited to share about the South Korean culture from its traditional and modern style. One of the interesting things that made Professor Budimansyah curious is about the Gangnam style in Korea. Check the full video here.",
         content:{
