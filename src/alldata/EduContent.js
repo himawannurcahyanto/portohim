b0da2d0dac3b82d0b9f4180d6ffb4693c4e7f224
himawannurcahyanto <himawanurcahyanto@gmail.com>
2023-05-07 14:48:13 +0700

    Update EduContent.js

--- src/alldata/EduContent.js
+++ src/alldata/EduContent.js
@@ -9,9 +9,17 @@ import sma1 from "../images/fotodirisma.jpg";
 
 export const EduData = [
     {
+        imgl: Ui,
+        edu: "Universitas Indonesia",
+        major:"Professional Engineering Degree Program (Ir.)",
+        year: "2022 - 2023",
+        desc:" Focus in implementation of engineering ethics, professionalism, safety (K3L), and Telecommunication study case for engineering. ",
+        imgr: Ui3,
+    },
+    {
         imgl: Kookmin,
         edu: "Kookmin University",
-        major:"Master of Engineering - M. Eng, Electrical Engineering ",
+        major:"Master of Engineering - M. Eng , Electrical Engineering ",
         year: "2020 - 2022",
         desc:"Graduate Student at Electrical and Electronic Engineering Department of Kookmin University, South Korea. Focusing on Renewable Energy, Internet of Energy (IoE) and Smart Grid.",
         imgr: kmu1,
@@ -19,7 +27,7 @@ export const EduData = [
     {
         imgl: Ui,
         edu: "Universitas Indonesia",
-        major:"Bachelor of Electrical Engineering",
+        major:"Bachelor of Electrical Engineering (S.T.)",
         year: "2015 - 2019",
         desc:"Undergraduate Student at Electrical Engineering Department of University of Indonesia. As Student Research Assistant at Electric Power and Energy Materials (EMAT), Focused on the strategy to optimize the performance of the generation, transmission, and utilization of electricity through technology.",
         imgr: Ui3,
